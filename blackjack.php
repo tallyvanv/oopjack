2d67f983a477acdcf1675c6361f024448b27d780
tallyvanv <talisa.vdv@gmail.com>
2020-02-13 12:32:33 +0100

    randomised score

--- blackjack.php
+++ blackjack.php
@@ -1,1 +1,27 @@
 <?php
+session_start();
+
+class Blackjack {
+    public $score = 0;
+
+    function hit(){
+        $randNum = rand(1,11);
+        $this->score = $_SESSION["score"];
+        $_SESSION["score"] = $this->score += $randNum;
+        echo $_SESSION["score"];
+        if ($this->score > 21) {
+            echo "<br>LOSER";
+            $_SESSION["score"] = 0;
+        }
+        return $_SESSION["score"];
+    }
+    function stand(){
+        echo "dealer's turn now";
+    }
+    function surrender(){
+        echo "you gave up";
+    }
+
+}
+
+require "game.php";
--- game.php
+++ game.php
@@ -1,1 +1,36 @@
 <?php
+$player = new Blackjack();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST["test"] == 1) {
+        $player->hit();
+    }
+    if ($_POST["test"] == 2) {
+        $player->stand();
+    }
+    if ($_POST["test"] == 3) {
+        $player->surrender();
+    }
+}
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <button type="submit" name="test" id="test" value="1">Hit me</button>
+    <button type="submit" name="test" id="test" value="2">Stand</button>
+    <button type="submit" name="test" id="test" value="3">Surrender</button>
+</form>
+</body>
+</html>
+
+
